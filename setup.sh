abb8dbf617340c8800b721f5e0f9a6a3cb1699f2
Markus Radike <markus.radike@3s-tec.at>
2024-02-14 17:59:28 +0100

    Thorium zu setup hinzugefügt

--- setup.sh
+++ setup.sh
@@ -60,3 +60,11 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\\\n ~/.local/share
 cd .dotfiles
 sudo stow .
 cd ~
+
+#install thorium browser
+sudo rm -fv /etc/apt/sources.list.d/thorium.list && \
+sudo wget --no-hsts -P /etc/apt/sources.list.d/ \
+http://dl.thorium.rocks/debian/dists/stable/thorium.list && \
+sudo apt update
+
+sudo nala install thorium-browser
